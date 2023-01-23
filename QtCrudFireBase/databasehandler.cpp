c0e0ca817c6488724e8eab9deb803fa090308d77
stescobedo <stescobedo.31@gmail.com>
2023-01-23 16:05:04 -0600

    Adding DatabaseHandler configuration

--- QtCrudFireBase/databasehandler.cpp
+++ QtCrudFireBase/databasehandler.cpp
@@ -3,5 +3,18 @@
 DatabaseHandler::DatabaseHandler(QObject *parent)
     : QObject{parent}
 {
+    m_networkManager = new QNetworkAccessManager(this);
+    m_networkReply = m_networkManager->get(QNetworkRequest(QUrl("https://qtfirebaseintegration-a2a67-default-rtdb.firebaseio.com/")));
 
+    connect(m_networkReply, &QNetworkReply::readyRead, this, &DatabaseHandler::networkReplyReadyRead);
+}
+
+DatabaseHandler::~DatabaseHandler()
+{
+    m_networkManager->deleteLater();
+}
+
+void DatabaseHandler::networkReplyReadyRead()
+{
+    qDebug() << m_networkReply->readAll();
 }
--- QtCrudFireBase/databasehandler.h
+++ QtCrudFireBase/databasehandler.h
@@ -2,14 +2,22 @@
 #define DATABASEHANDLER_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QDebug>
 
 class DatabaseHandler : public QObject
 {
     Q_OBJECT
 public:
     explicit DatabaseHandler(QObject *parent = nullptr);
+    ~DatabaseHandler();
 
 signals:
+
+private:
+    QNetworkAccessManager* m_networkManager;
+    QNetworkReply* m_networkReply;
 
 };
 
